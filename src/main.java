1e8321a360000d2f3a3ad074cf56f29f47f3c416
Avenel <martin.briewig@googlemail.com>
2013-03-12 19:05:54 +0100

    Setup basic JavaFX application

---
+++ src/main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class main extends Application{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+		System.out.println("Hello World");
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("JavaCity - v.0.1a");
+		stage.setWidth(800);
+		stage.setHeight(600);
+		
+		stage.show();
+	}
+	
+
+}
